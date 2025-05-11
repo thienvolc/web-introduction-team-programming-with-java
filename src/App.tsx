d51fe9b7f2b5e0d049a97589aca0fa40d3299042
thienvolc <ttt2042006@gmail.com>
2025-05-11 16:54:15 +0700

    reset change for github page 💩

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
 import { Home } from './pages/Home';
@@ -8,7 +8,7 @@ import { Design } from './pages/Design';
 import { Tech } from './pages/Tech';
 import { MinutesPage } from './pages/minutes/MinutesPage';
 export function App() {
-  return <Router basename={`/${process.env.GITHUB_PROJECT_NAME}`}>
+  return <Router>
       <div className="flex flex-col min-h-screen bg-background">
         <Navbar />
         <main className="flex-grow">
